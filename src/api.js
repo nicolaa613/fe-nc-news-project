47e11508520742305183860585dcda87cef0a988
nicolaa613 <nicola.jones.94@btinternet.com>
2024-09-19 11:36:23 +0100

    add voting component to articles

--- src/api.js
+++ src/api.js
@@ -5,25 +5,26 @@ const ncNewsApi = axios.create({
 });
 
 export const getArticles = () => {
-  return ncNewsApi
-    .get("/articles")
-    .then((articles) => {
-      return articles.data;
-    })
+  return ncNewsApi.get("/articles").then((articles) => {
+    return articles.data;
+  });
 };
 
 export const getArticle = (article_id) => {
-  return ncNewsApi
-    .get(`/articles/${article_id}`)
-    .then((requestedArticle) => {
-      return requestedArticle.data;
-    })
+  return ncNewsApi.get(`/articles/${article_id}`).then((requestedArticle) => {
+    return requestedArticle.data;
+  });
 };
 
 export const getArticleComments = (article_id) => {
   return ncNewsApi
-  .get(`/articles/${article_id}/comments`)
-  .then((requestedComments) => {
-    return requestedComments.data
-  })
-}
+    .get(`/articles/${article_id}/comments`)
+    .then((requestedComments) => {
+      return requestedComments.data;
+    });
+};
+
+export const patchVote = (article_id, vote) => {
+  const voteUpdate = { inc_votes: vote };
+  return ncNewsApi.patch(`/articles/${article_id}`, voteUpdate);
+};
--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,21 +1,31 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { getArticle } from "../api";
+import { getArticle, patchVote } from "../api";
 import Comments from "./Comments"
 
 const Article = () => {
   const { article_id } = useParams();
   const [article, setArticle] = useState({});
+  const [votes, setVotes] = useState()
 
   useEffect(() => {
     getArticle(article_id).then((article) => {
-        setArticle(article)
+      setArticle(article)
+      setVotes(article.votes)
     })
   }, [article_id])
 
+  const handleVote = ((vote) => {
+    setVotes((previousCount) => previousCount + vote)
+    patchVote(article_id, vote)
+  })
+  
   return (
     <section>
         <h2>{article.title}</h2>
+        <h3>{votes} votes 
+        <button className="vote-button" onClick={() => handleVote(-1)}>-</button>
+        <button className="vote-button" onClick={() => handleVote(1)}>+</button></h3>
         <img src={article.article_img_url} />
         <h4>{article.author} | Published: {article.created_at}</h4>
         <p>{article.body}</p>
